55fa9ea359a7bedf5a678eecf9aeb8bb098fa688
ayushisinhapearl <ayushisinha@pearlorganisation.com>
2025-03-20 16:17:08 +0530

    clyzo figma design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Banner from './component/banner'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div >
+      <Banner />
+      <div className="h-[200vh] bg-black"></div>
+    </div>
   )
 }
 
