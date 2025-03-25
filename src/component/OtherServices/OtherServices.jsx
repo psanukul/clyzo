5f7d300b739996522378732ebc1c875112625a0a
Anonymous <rahul@pearlorganisation.com>
2025-03-25 17:12:50 +0530

    add animation

--- src/component/OtherServices/OtherServices.jsx
+++ src/component/OtherServices/OtherServices.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(scroll)
 
 const OtherServices = () => {
+  const textContainerRef = useRef(ScrollTrigger);
+
+  useEffect(() => {
+    const textElements = gsap.utils.toArray(".animated-text");
+
+    gsap.fromTo(
+      textElements,
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.5,
+        ease: "power3.out",
+        delay: 0.5, // 2-second wait before animation starts
+        stagger: 0.3, // Each text animates 0.3s apart
+        scrollTrigger: {
+          trigger: textContainerRef.current,
+          start: "top 75%",
+          
+          toggleActions: "play none none none",
+        },
+      }
+    );
+  }, []);
+ 
+
   return (
     <div className="bg-white mt-5 w-full  -z-3 ">
       <div className="flex flex-col justify-center gap-0 px-1 lg:px-40 lg:py-1">
@@ -37,22 +68,23 @@ const OtherServices = () => {
             className="relative h-[80vh] w-auto object-contain items-start"
           />
         </div>
-        <div className="absolute  mt-[30vh] sm:mt-[18vh] md:mt-[12vh] h-[30vh] sm:h-[45vh] md:h-[50vh] lg:h-[50vh] w-[68%] md:w-[38%] text-white z-20 font-bold flex flex-col items-center justify-start text-[10px] sm:text-[20px] md:text-sm">
+        <div ref={textContainerRef}
+ className="absolute  mt-[30vh] sm:mt-[18vh] md:mt-[12vh] h-[30vh] sm:h-[45vh] md:h-[50vh] lg:h-[50vh] w-[68%] md:w-[38%] text-white z-20 font-bold flex flex-col items-center justify-start text-[10px] sm:text-[20px] md:text-sm">
     
-         <p className=" text-[2vh] sm:text-[3vh] md:text-[5vh] mb-1 md:mb-6 font-semibold">REGpro <span className="text-[2vh] sm:text-[3vh] md:text-[10vh] ">..</span></p>
-          <div className="w-full">
-            <p>What’s easier than </p>
-            <p className=" text-[2vh] sm:text-[8vh] md:text-[10vh] py-0 md:py-6  font-normal">ONE CLICK</p>
+         <p className=" animated-text text-[2vh] sm:text-[3vh] md:text-[5vh] mb-1 md:mb-6 font-semibold">REGpro <span className="text-[2vh] sm:text-[3vh] md:text-[10vh] ">..</span></p>
+          <div className="w-full animated-text">
+            <p >What’s easier than </p>
+            <p className="text-[2vh] sm:text-[8vh] md:text-[10vh] md:py-6 font-normal py-0 my-0">ONE CLICK</p>
           </div>
 
-          <p className="text-[1.6vh] md:text-[3vh] font-bold pl-10"> for all your regulatory document needs ?</p>
-          <button className="border border-2px border-white bottom-4 md:bottom-6 mb-32 sm:mb-14 mt-auto text-[1.3vh] md:text-[10px] px-2 shadow-sm shadow-white md:px-3 hover:text-green-500 hover:border-green-500 hover:scale-105 duration-300">Know more</button>
+          <p className="text-[1.6vh] md:text-[3vh] font-bold pl-10 animated-text"> for all your regulatory document needs ?</p>
+          <button className="border border-2px border-white bottom-4 md:bottom-6 mb-32 sm:mb-14 mt-auto text-[1.3vh] md:text-[10px] px-2 shadow-sm shadow-white md:px-3 hover:text-green-500 hover:border-green-500 hover:scale-105 duration-300 animated-text  ">Know more</button>
         </div>
       </div>
 
 
    <div className="flex  flex-col md:flex-row gap-4 md:gap-24 items-center justify-center w-full h-auto md:h-[20vh] bg-[#006761] text-white px-6 md:px-20">
-     <h1 className=" text-[3vh] md:text-[5vh] w-50 md:w-40 font-semibold space-x-2">INSTANT QUOTES ..</h1>
+     <h1 className=" text-[3vh] md:text-[5vh] w-50 md:w-40 font-semibold space-x-2">INSTANT QUOTES ...</h1>
      <p className=" text-[2vh] md:text-[3vh] md:font-semibold">Get Instant Quotes, Save Time, and Make Quick Decisions </p>
      <button className="border border-2px border-white  text-[2vh] md:text-sm px-2 mb-5 md:px-3 hover:text-green-500 hover:border-green-500 hover:scale-105 duration-300"> Know more</button >
    </div>
--- src/component/PopularExcipients/PopularExcipients.jsx
+++ src/component/PopularExcipients/PopularExcipients.jsx
@@ -3,46 +3,92 @@ import { gsap } from "gsap";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const cardsData = [
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine.jpg", title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine1.jpg", title: "Amine Fluoride 33% (Permafluor)" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Dichloromethane stabilized with ~ 20 ppm of amylene (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine2.jpg", title: "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade" },
-  { bgImage: "/GlobalPrinciple/BgImages/medicine3.jpg", title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade" },
+  {
+    bgImage: "/PopularExcipients/2-propanol.png",
+    title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/AmineFluoride.png",
+    title: "Amine Fluoride 33% (Permafluor)",
+  },
+  {
+    bgImage: "/PopularExcipients/ArtificialPowderedLiver.png",
+    title: "Artificial Powdered Flavor (Liver) Pharmaceutical Grade",
+  },
+  {
+    bgImage: "/PopularExcipients/ArtificialPowderedChicken.png",
+    title:
+      "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/ArtificialPowderedBeef.png",
+    title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade",
+  },
+  {
+    bgImage: "/PopularExcipients/BenzylAlcohol.png",
+    title: "Benzyl Alcohol (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/BoricAcid.png",
+    title: "Boric Acid EP, USP (tested for BET/MLT)",
+  },
+  {
+    bgImage: "/PopularExcipients/ChlorobutanolAnhydrous.png",
+    title: "Chlorobutanol Anhydrous Ph.Eur, USP, NF (Injectable Grade)",
+  },
+  {
+    bgImage: "/PopularExcipients/ChlorobutanolHemihydrate.png",
+    title:
+      "Chlorobutanol Hemihydrate, Sifted Ph. Eur, USP, NF (Injectable Grade)",
+  },
+  {
+    bgImage: "/PopularExcipients/Chlorocresol.png",
+    title:
+      "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/2-propanol.png",
+    title: "2-Propanol (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/AmineFluoride.png",
+    title: "Amine Fluoride 33% (Permafluor)",
+  },
+  {
+    bgImage: "/PopularExcipients/ArtificialPowderedLiver.png",
+    title: "Artificial Powdered Flavor (Liver) Pharmaceutical Grade",
+  },
+  {
+    bgImage: "/PopularExcipients/ArtificialPowderedChicken.png",
+    title:
+      "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/ArtificialPowderedBeef.png",
+    title: "Artificial Powdered Flavor (Chicken) Pharmaceutical Grade",
+  },
+  {
+    bgImage: "/PopularExcipients/BenzylAlcohol.png",
+    title: "Benzyl Alcohol (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
+  {
+    bgImage: "/PopularExcipients/BoricAcid.png",
+    title: "Boric Acid EP, USP (tested for BET/MLT)",
+  },
+  {
+    bgImage: "/PopularExcipients/ChlorobutanolAnhydrous.png",
+    title: "Chlorobutanol Anhydrous Ph.Eur, USP, NF (Injectable Grade)",
+  },
+  {
+    bgImage: "/PopularExcipients/ChlorobutanolHemihydrate.png",
+    title:
+      "Chlorobutanol Hemihydrate, Sifted Ph. Eur, USP, NF (Injectable Grade)",
+  },
+  {
+    bgImage: "/PopularExcipients/Chlorocresol.png",
+    title:
+      "Chlorocresol (4-Chloro-3-Methylphenol) (USP-NF, BP, Ph. Eur.) pure, pharma grade",
+  },
 ];
 
 export default function CategoryGrid() {
@@ -53,7 +99,13 @@ export default function CategoryGrid() {
   useEffect(() => {
     const updateItemsPerPage = () => {
       setItemsPerPage(
-        window.innerWidth < 480 ? 2 : window.innerWidth < 768 ? 3 : window.innerWidth < 1024 ? 4 : 8
+        window.innerWidth < 480
+          ? 3
+          : window.innerWidth < 768
+          ? 4
+          : window.innerWidth < 1024
+          ? 6
+          : 10
       );
     };
     updateItemsPerPage();
@@ -72,7 +124,10 @@ export default function CategoryGrid() {
   }, [currentPage]);
 
   const totalPages = Math.ceil(cardsData.length / itemsPerPage);
-  const currentItems = cardsData.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
+  const currentItems = cardsData.slice(
+    currentPage * itemsPerPage,
+    (currentPage + 1) * itemsPerPage
+  );
 
   const handleNextPage = () => {
     if (gridRef.current?.children.length) {
@@ -115,18 +170,19 @@ export default function CategoryGrid() {
             </div>
           </div>
           <h1 className="text-3xl font-bold">
-          Most 
+            Most
             <span className="text-[#22604A]">Popular Excipients</span>
           </h1>
         </div>
 
         <p className="text-regular text-[2vh] font-normal pb-8 p-2 pl-10">
-        Here is the list of the most popular excipients that are commonly searched for and used in the pharmaceutical industry
+          Here is the list of the most popular excipients that are commonly
+          searched for and used in the pharmaceutical industry
         </p>
       </div>
 
       <div className="relative w-full px-20">
-        {currentPage < totalPages && (
+        {currentPage > 0&& (
           <button
             onClick={handlePrevPage}
             className="absolute left-9 top-1/2   -translate-y-1/2 z-10 bg-gray-300 p-2 rounded-full shadow-md"
@@ -134,31 +190,41 @@ export default function CategoryGrid() {
             <ChevronLeft className="w-6 h-6" />
           </button>
         )}
-        <div ref={gridRef} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-  {currentItems.map((card, index) => (
-    <div key={index} className="flex flex-col justify-center items-center overflow-hidden cursor-pointer  bg-gradient-to-b from-[#bbbbbb] to-[#ffffff]">
-      
-      {/* Image Container */}
-      <div className="relative w-full h-40 group">
-        <img src={card.bgImage} alt={card.title} className="w-full h-full object-cover transition-all duration-300" />
-        
-        {/* Gradient Overlay Applied Only to Image */}
-        <div className="absolute inset-0 bg-gradient-to-t from-[#52b295b9] to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-      </div>
+        <div
+          ref={gridRef}
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6"
+        >
+          {currentItems.map((card, index) => (
+            <div
+              key={index}
+              className="flex flex-col justify-center items-center overflow-hidden cursor-pointer "
+            >
+              {/* Image Container */}
+              <div className="relative w-full h-40 group">
+                <img
+                  src={card.bgImage}
+                  alt={card.title}
+                  className="w-full h-full  transition-all duration-300 object-contain" 
+                />
 
-      {/* Content */}
-      <div className="w-full h-20 flex flex-col justify-center items-center">
-        <p className="mt-2 w-full text-[10px] font-normal text-start px-2">{card.title}</p>
-        <button className="mt-auto mb-1 border-2 border-green-700 w-20 text-center text-xs text-green-700 rounded-full">
-          Buy Now
-        </button>
-      </div>
+                {/* Gradient Overlay Applied Only to Image */}
+                <div className="absolute inset-0 bg-gradient-to-t from-[#52b295b9] to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+              </div>
 
-    </div>
-  ))}
-</div>
+              {/* Content */}
+              <div className="w-full h-20 flex flex-col justify-center items-center  bg-gradient-to-b from-[#e9e9e9] to-[#ffffff]">
+                <p className="mt-2 w-full text-[10px] font-normal text-start px-2">
+                  {card.title}
+                </p>
+                <button className="mt-auto mb-1 border-2 border-green-700 w-20 text-center text-[10px] text-green-700 rounded-full p-1">
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
 
-        {totalPages > currentPage && (
+        {totalPages - 1 > currentPage && (
           <button
             onClick={handleNextPage}
             className="absolute right-9 top-1/2 -translate-y-1/2 z-10 bg-gray-300 p-2 rounded-full shadow-md"
